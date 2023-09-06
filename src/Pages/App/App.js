67732d43569e14bd216886d6561b148d06124453
Erwin Medina <erwin.l.medina@gmail.com>
2023-09-06 14:41:38 -0700

    The first commit

---
+++ src/Pages/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello and Welcome</h1>
+      <h2>Testing.. Testing</h2>
+      <h3>This is</h3>
+      <h2>SPORTS TRACKER</h2>
+    </div>
+  );
+}
+
+export default App;
